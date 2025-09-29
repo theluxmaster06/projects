1f859da9616e0dc58d542e5c417a9581f6ab3f10
theluxmaster06 <theluxmaster06@gmail.com>
2025-09-29 10:09:02 +0200

    sorting

---
+++ independed/side project/side project game fr.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+
+
+# Main game loop
+print("would u like to start?")
+start = input("yes or no?").lower().strip()
+if start == ("yes"):
+    print ("game starting.")
+    print ("game starting..")
+    print ("game starting...")
+    x = 1
+
+elif start == ("no"):
+    print("game not starting")
+
+else:
+    print ("invalid argument")
+
+
+
+if x == 1:
+    running = True
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    
+    
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+pygame.quit()
+
+# This is a simple Pygame window that runs until closed by the user.
